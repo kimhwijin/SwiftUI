dd82dcd2e549b373fc7be277ce1909394494ab13
kimhwijin <rlagnlwls0160@gmail.com>
2021-11-15 00:04:29 +0900

    Lottie Size Change, Background Linear Gradient Color

--- LotteExercise/LotteExercise/ContentView.swift
+++ LotteExercise/LotteExercise/ContentView.swift
@@ -24,7 +24,21 @@ struct feelingEmojiView: View{
         
         VStack{
             LottieAnimationview(jsonFile: "feeling_emoji")
+            //직접 애니매이션 뷰의 lottie 이미지 사이즈를 조정 불가능하다...
+            //UIView로 덮어서 사이즈를 조정할 수 있다.
+                //.frame(width: 300, height: 300)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            LinearGradient(
+                colors: [
+                    Color.green,
+                    Color.blue,
+                    Color.pink,
+                ],
+                startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+        )
         
     }
 }
--- LotteExercise/LotteExercise/View/LottieAnimationview.swift
+++ LotteExercise/LotteExercise/View/LottieAnimationview.swift
@@ -11,13 +11,35 @@ import Lottie
 struct LottieAnimationview: UIViewRepresentable {
 
     var jsonFile: String
-    func makeUIView(context: Context) -> AnimationView {
+    @Binding var progress: CGFloat
+    func makeUIView(context: Context) -> UIView {
+        
+        
+        //UIView 를 만들고, lottie 이미지를 센터에 위치시킨다.
+        let rootView = UIView()
+        rootView.backgroundColor = .clear
+
+        //lottie 이미지를 위치시키는 애니메이션 뷰
         let animationView = AnimationView(name: jsonFile, bundle: .main)
         animationView.backgroundColor = .clear
-        return animationView
+        animationView.tag = 1009
+        //뷰 사이즈가 제약에 맞춰서 리사이징 되는것을 막음
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //lottie 이미지가 들어가있는 애니메이션 뷰와 그걸 덮는 UIView의 크기를 일치시켜서 lottie 이미지 사이즈를 조정한다.
+        let constraints = [
+            animationView.widthAnchor.constraint(equalTo: rootView.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: rootView.heightAnchor),
+        ]
+        
+        rootView.addSubview(animationView)
+        rootView.addConstraints(constraints)
+        
+        return rootView
+        
     }
     
-    func updateUIView(_ uiView: AnimationView, context: Context) {
+    func updateUIView(_ uiView: UIView, context: Context) {
         
     }
 }
