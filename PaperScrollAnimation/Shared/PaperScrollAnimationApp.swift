c7b539c06b58656e3ca804ce39c9716281639a76
kimhwijin <rlagnlwls0160@gmail.com>
2021-11-23 16:44:43 +0900

    paper scroll animation init

---
+++ PaperScrollAnimation/Shared/PaperScrollAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperScrollAnimationApp.swift
+//  Shared
+//
+//  Created by 김휘진 on 2021/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PaperScrollAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
