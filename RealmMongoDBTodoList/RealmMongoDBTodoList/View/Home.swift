c12be96cb3546f9872ef1ec2baac2914b2a377f3
kimhwijin <rlagnlwls0160@gmail.com>
2021-12-30 22:51:30 +0900

    add task item

--- RealmMongoDBTodoList/RealmMongoDBTodoList/View/Home.swift
+++ RealmMongoDBTodoList/RealmMongoDBTodoList/View/Home.swift
@@ -23,14 +23,19 @@ struct Home: View {
                 }
                 else{
                     List{
-                        
+                        ForEach(tasksFetched){task in
+                            TaskRow(task: task)
+                        }
                     }
+                    .listStyle(.insetGrouped)
                 }
             }
             .navigationTitle("Task's")
             .toolbar{
                 Button{
-                    
+                    //버튼 클릭시 Realm Object 생성
+                    let task = TaskItem()
+                    $tasksFetched.append(task)
                 } label: {
                     Image(systemName: "plus")
                 }
@@ -43,4 +48,57 @@ struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
     }
+}
+
+struct TaskRow: View{
+    
+    @ObservedRealmObject var task: TaskItem
+    
+    var body: some View{
+        
+        //Task Status Indicator Menu
+        HStack(spacing: 15){
+            Menu{
+                //Update Date
+                Button("Missed"){
+                    $task.taskStatus.wrappedValue = .missed
+                }
+                Button("Completed"){
+                    $task.taskStatus.wrappedValue = .completed
+                }
+            } label: {
+                Circle()
+                    .stroke(.gray)
+                    .frame(width: 15, height: 15)
+                    .overlay(
+                        Circle()
+                            .fill(task.taskStatus == .missed ? .red :
+                                    (task.taskStatus == .pending ? .yellow : .green))
+                    
+                    )
+            }
+            VStack(alignment: .leading, spacing: 12){
+                
+                TextField("Refresh", text: $task.taskTitle)
+                
+                if task.taskTitle != ""{
+                    
+                    Picker(selection: .constant("")){
+                        DatePicker(selection: $task.taskDate, displayedComponents: .date){
+                            
+                        }
+                        .labelsHidden()
+                        .navigationTitle("Task Date")
+                    } label: {
+                        HStack{
+                            Image(systemName: "calendar")
+                            Text(task.taskDate.formatted(date: .abbreviated, time: .omitted))
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+    
 }
